95d86f332560738af4b08895489d32cee1d344db
Naila Syed <naila15june@gmail.com>
2025-10-17 13:55:35 +0530

    upadte 4.5.2

--- client/src/api.js
+++
@@ -1,6 +0,0 @@
-const API_URL = 'https://rtspstream-dragable-overlay-backend.vercel.app';
-
-export async function getOverlays() {
-    const response = await fetch(`${API_URL}/overlays`);
-    return response.json();
-}
