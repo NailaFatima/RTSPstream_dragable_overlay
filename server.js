54ab114296d4cafa7c415b8826ce11884eade0fe
Naila Syed <naila15june@gmail.com>
2025-10-17 19:43:34 +0530

    update7.2

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 
-const allowedOrigins = [
-  "http://localhost:3001", // ✅ Local dev
-  "https://rtspstream-dragable-overlay-backend.vercel.app" // ✅ Your deployed frontend
-];
+// const allowedOrigins = [
+//   "http://localhost:3001", // ✅ Local dev
+//   "https://rtspstream-dragable-overlay-backend.vercel.app" // ✅ Your deployed frontend
+// ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: allowedOrigins,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// }));
 
 
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
